474f167cfd869ad72fa1c2d387c7e44d53b6e24b
Micke <mate310173@gmail.com>
2016-06-18 21:21:22 -0500

    Initial Commit

---
+++ HamburquesasEnElMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburquesasEnElMundo
+//
+//  Created by Miguel Angel Trejo Escamilla on 18/06/16.
+//  Copyright © 2016 Miguel Angel Trejo Escamilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
